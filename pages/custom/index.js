b580a601baf7ad4f7985e31c4d5f78707b4fcc40
niyaochengxianle <1130205766@qq.com>
2023-04-10 17:02:28 +0800

    客户扫码后填写的页面

--- pages/custom/index.js
+++ pages/custom/index.js
@@ -5,9 +5,33 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    show:false,
+    showPicker:false,
+    areaList:[],
+    columns:[
+      {text:'已分配'},
+      {text:'未分配'}
+    ],
   },
-
+  //打开选择市区
+  showPopup() {
+    this.setData({ show: true });
+  },
+  // 关闭选择市区
+  onClose() {
+    this.setData({ show: false });
+  },
+  confirmArea(){},
+  cancelArea(){},
+    // 关闭选择状态
+  closePicker() {
+    this.setData({ showPicker: false });
+  },
+  showPicker(){
+    this.setData({ showPicker: true });
+  },
+  confirmPicker(){},
+  cancelPicker(){},
   /**
    * 生命周期函数--监听页面加载
    */
